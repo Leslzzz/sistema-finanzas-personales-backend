b41314a25fa13547b4605c2025f26a14219a8adb
Lesly Jimenez <leslzzzzzz@gmail.com>
2026-02-13 13:23:32 -0600

    se modifico para resolver el error de core-urls

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import RegisterSerializer, MyTokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -9,17 +13,11 @@ class RegisterView(generics.CreateAPIView):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-
 class DashboardHomeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         user = request.user
-        
         return Response({
             "user_info": {
                 "name": user.name,
