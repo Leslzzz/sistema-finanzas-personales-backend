b34e841dabd5bf22b07d736e2eb9bef41a1c825d
Lesly Jimenez <leslzzzzzz@gmail.com>
2026-03-22 21:08:39 -0700

    se agrego lo dwe categorias

--- finanzas/views.py
+++ finanzas/views.py
@@ -1,11 +1,21 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import Transaction
-from .serializers import TransactionSerializer
+from .models import Transaction, Category
+from .serializers import TransactionSerializer, CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Cada usuario solo ve sus propias categorías
+        return Category.objects.filter(user=self.request.user)
+
 
 class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Transaction.objects.filter(user=self.request.user)
+        return Transaction.objects.filter(user=self.request.user) 
